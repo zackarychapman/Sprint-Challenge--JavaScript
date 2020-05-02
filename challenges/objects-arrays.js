45cb544618f7c1d915a29294d9f4a2c3c08551f5
zack <zackarychapman00@gmail.com>
2020-05-02 11:51:56 -0400

    passed up to advanced array methods minus roar method logging correctly.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,28 +7,52 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+const tyrannosaurus = {
+  name: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceous',
+  roar: function scaryRoar () {
+    return "RAWERSRARARWERSARARARRRR!";
+  }
+}
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
+const stegosaurus = {
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic',
+}
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+const velociraptor = {
+  name: 'carnivorous',
+  diet: 'herbivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceous',
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
+
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus.roar);//THIS ONE NEEDS JUST A LITTLE HELP!//
 
 
 // ==== Arrays ====
@@ -52,6 +76,7 @@ const graduates = [
 
 Once you have the new array created, log the result. */
 const universities = [];
+graduates.forEach(item => {university.push(item.universities)};
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -61,12 +86,19 @@ The resulting contact information strings should have a space between the first 
 
 Log the result of your new array. */
 const contactInfo = [];
+graduates.forEach(item => {
+  emails.push(`${item.name}, ${item.email}`)
+});
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
 console.log(unisWithUni);
-
+graduates.forEach(item => {
+  if(item.university.includes('Uni')) {
+    unisWithUni.push(item);
+  }
+})
 
 // ==== ADVANCED Array Methods ====
 
